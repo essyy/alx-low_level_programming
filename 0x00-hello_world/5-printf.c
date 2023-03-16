c54fc0d468f5d009da4ceee114546a0d97cdc233
essyy <esther.mbugua@strathmore.edu>
2023-03-16 15:16:49 -0700

    task Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,7 @@
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art /n");
+printf("with proper grammar, but the outcome is a piece of art,");
+printf("/n");
 return (0);
 }
