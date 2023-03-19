26d4d821403377db45adb003cc8fa69e057c1003
essyy <esther.mbugua@strathmore.edu>
2023-03-19 13:45:50 -0700

    Numbers task 5

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/* main - Entry point
+ *
+ *Return: Success is 0 (Always)
+ *
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if ()
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Function that prints numbers zero to ten
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	while (a <= 10)
+	{
+		printf("%d \n", a);
+			a++;
+	}
+}
