a9aeb538cf2ce983afed426e2a1eaa5c96d98e14
essyy <esther.mbugua@strathmore.edu>
2023-03-21 11:16:21 -0700

    Function that prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints printchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar(_putchar);
+	putchar('\n');
+	return (0);
+}
