5f38c28ba7ecb236365e8fc75b126e0fbd22b061
essyy <Esther.mbugua@strathmore.edu>
2023-03-23 21:42:52 +0300

    task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  more_numbers -  function that prints 10 times the numbers
+ *
+ */
+void more_numbers(void)
+{
+	int c, i;
+
+	for (c = 1; c <= 10; c++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i);
+		}
+	 _putchar('\n');
+	}
+}
