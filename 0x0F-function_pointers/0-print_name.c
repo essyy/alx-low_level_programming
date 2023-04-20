3f99a9e5169400e5d216b944cd8adbf9d104b5fc
essyy <Esther.mbugua@strathmore.edu>
2023-04-20 23:39:11 +0300

    tasks done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - function that prints name
+ * @name: the character name
+ * @f: parameters inteh function
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
